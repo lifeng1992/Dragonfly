75b11676836e460f08ad21d8f3c96eb0378c084b
lifeng1335 <lifeng1335@outlook.com>
2016-05-20 07:20:12 +0800

    save

--- Dragonfly/app/src/main/java/com/bamcop/dragonfly/MainActivity.java
+++ Dragonfly/app/src/main/java/com/bamcop/dragonfly/MainActivity.java
@@ -3,6 +3,8 @@ package com.bamcop.dragonfly;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.alorma.github.sdk.services.client.GithubClient;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
